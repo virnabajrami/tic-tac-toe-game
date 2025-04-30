061e88819ae21f97314dab949786a24026f6b790
virnabajrami <50464607+virnabajrami@users.noreply.github.com>
2025-04-30 12:53:01 +0200

    Add files via upload

---
+++ src/components/GameOver.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function GameOver({ winner, onRestart }) {
+  return (
+    <div id="game-over">
+      <h2>Game Over</h2>
+      {winner && <p>{winner} won!</p>}
+      {!winner && <p>It&apos;s a draw!</p>}
+      <button onClick={onRestart}>Rematch!</button>
+    </div>
+  );
+}
---
+++ src/components/Player.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
+  }
+
+  function handleChange(event) {
+    console.log(event);
+    setPlayerName(event.target.value);
+  }
+
+  let editableplayerName = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    editableplayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+  }
+
+  return (
+    <li className={isActive ? "active" : ""}>
+      <span className="player">
+        {editableplayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
